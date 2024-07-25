73b3623e9cfee6a0bf71a98478cb96334dcee5cb
SimplyAddons <116991888+SimplyAddons@users.noreply.github.com>
2024-07-25 00:40:02 -0400

    safer config handling

--- Core/2-Database.lua
+++ Core/2-Database.lua
@@ -1,11 +1,11 @@
 local E = select(2, ...):unpack()
 
 function E:GetConfig(key)
-    --return Config[key]
-    return self.DB.global[key]
+	return self.DB and self.DB.global[key]
 end
 
 function E:SaveConfig(key, value)
-    --Config[key] = value
-    self.DB.global[key] = value
+	if self.DB then
+		self.DB.global[key] = value
+	end
 end
