a66dc12df877dcb597db8925b668e3c46d462752
SimplyAddons <116991888+SimplyAddons@users.noreply.github.com>
2024-07-25 00:39:10 -0400

    use blizzards new options registration

--- Config/2-Options.lua
+++ Config/2-Options.lua
@@ -1,29 +1,127 @@
 local E, Config = select(2, ...):unpack()
 
--- table of available options
+--[[
 function E:Options()
+	local options = {
+		name = "TidyLFG Options",
+		type = "group",
+		args = {
+			general = {
+				type = "group",
+				name = "General Settings",
+				order = 1,
+				args = {
+					showLogs = {
+						type = "toggle",
+						name = "Print TidyLFG logs to chat window",
+						width = "full",
+						order = 1,
+						get = function(info) return E:GetConfig("showLogs") end,
+						set = function(info, value) E:SaveConfig("showLogs", value) end,
+					},
+				},
+			},
+			filter = {
+				type = "group",
+				name = "Filter Settings",
+				order = 2,
+				args = {
+					filterUS = {
+						type = "toggle",
+						name = "Filter US realms from the dungeon list",
+						width = "full",
+						order = 1,
+						get = function(info) return E:GetConfig("filterUS") end,
+						set = function(info, value) E:SaveConfig("filterUS", value) end,
+					},
+					filterOCE = {
+						type = "toggle",
+						name = "Filter OCE realms from the dungeon list",
+						width = "full",
+						order = 2,
+						get = function(info) return E:GetConfig("filterOCE") end,
+						set = function(info, value) E:SaveConfig("filterOCE", value) end,
+					},
+					filterBrazil = {
+						type = "toggle",
+						name = "Filter Brazil realms from the dungeon list",
+						width = "full",
+						order = 3,
+						get = function(info) return E:GetConfig("filterBrazil") end,
+						set = function(info, value) E:SaveConfig("filterBrazil", value) end,
+					},
+					filterLatin = {
+						type = "toggle",
+						name = "Filter Latin realms from the dungeon list",
+						width = "full",
+						order = 4,
+						get = function(info) return E:GetConfig("filterLatin") end,
+						set = function(info, value) E:SaveConfig("filterLatin", value) end,
+					},
+				},
+			},
+		},
+	}
+	return options
+end
+]]
 
-    local options = {}
-    options[1] = {
-        key = "showLogs",
-        desc = "  Print TidyLFG logs to chat window"
-    }
-    options[2] = {
-        key = "filterUS",
-        desc = "  Filter US realms from the dungeon list"
-    }
-    options[3] = {
-        key = "filterOCE",
-        desc = "  Filter OCE realms from the dungeon list"
-    }
-    options[4] = {
-        key = "filterBrazil",
-        desc = "  Filter Brazil realms from the dungeon list"
-    }
-    options[5] = {
-        key = "filterLatin",
-        desc = "  Filter Latin realms from the dungeon list"
-    }
-
-    return options
+function E:Options()
+	local options = {
+		name = "TidyLFG Options",
+		type = "group",
+		args = {
+			header1 = {
+				type = "header",
+				name = "General",
+				order = 1,
+			},
+			showLogs = {
+				type = "toggle",
+				name = "Print TidyLFG logs to chat window",
+				width = "full",
+				order = 2,
+				get = function(info) return E:GetConfig("showLogs") end,
+				set = function(info, value) E:SaveConfig("showLogs", value) end,
+			},
+			header2 = {
+				type = "header",
+				name = "Filters",
+				order = 3,
+			},
+			filterUS = {
+				type = "toggle",
+				name = "Filter US realms from the dungeon list",
+				width = "full",
+				order = 4,
+				get = function(info) return E:GetConfig("filterUS") end,
+				set = function(info, value) E:SaveConfig("filterUS", value) end,
+			},
+			filterOCE = {
+				type = "toggle",
+				name = "Filter OCE realms from the dungeon list",
+				width = "full",
+				order = 5,
+				get = function(info) return E:GetConfig("filterOCE") end,
+				set = function(info, value) E:SaveConfig("filterOCE", value) end,
+			},
+			filterBrazil = {
+				type = "toggle",
+				name = "Filter Brazil realms from the dungeon list",
+				width = "full",
+				order = 6,
+				get = function(info) return E:GetConfig("filterBrazil") end,
+				set = function(info, value) E:SaveConfig("filterBrazil", value) end,
+			},
+			filterLatin = {
+				type = "toggle",
+				name = "Filter Latin realms from the dungeon list",
+				width = "full",
+				order = 7,
+				get = function(info) return E:GetConfig("filterLatin") end,
+				set = function(info, value) E:SaveConfig("filterLatin", value) end,
+			},
+		},
+	}
+	return options
 end
--- Config/3-Core.lua
+++ Config/3-Core.lua
@@ -1,47 +1,60 @@
 local E = select(2, ...):unpack()
 
 function E:InitOptions()
+	-- Create options panel frame
+	E.optionsPanel = CreateFrame("Frame", nil, UIParent)
+	E.optionsPanel.name = E.AddOn
+	E.optionsPanel:Hide()
 
-    -- create options panel frame
-    E.optionsPanel = CreateFrame("Frame", nil, UIParent)
-    E.optionsPanel.name = E.AddOn
-    E.optionsPanel:Hide()
-
-    -- create button frames
-    -- note: we attach the options to the panel now, rather than onShow,
+	-- Create button frames
+	-- note: we attach the options to the panel now, rather than onShow,
     -- to prevent errors if the user attempts to toggle the logs before
     -- opening the options window
-    local options = E:Options()
-    for id=1, #options do
-        E.optionsPanel[options[id]["key"]] = CreateFrame("CheckButton", nil, E.optionsPanel, "UICheckButtonTemplate");
-    end
+	local options = E:Options()
+	for id=1, #options do
+		E.optionsPanel[options[id]["key"]] = CreateFrame("CheckButton", nil, E.optionsPanel, "UICheckButtonTemplate")
+	end
 
-    E.optionsPanel:SetScript("OnShow", function(self)
+	E.optionsPanel:SetScript("OnShow", function(self)
+		-- Grab our options
+		local options = E:Options()
 
-        -- grab our options
-        local options = E:Options()
+		-- Title
+		local title = E.optionsPanel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+		title:SetPoint("TOPLEFT", 16, -16)
+		title:SetText(E.AddOn .. ' Options')
 
-        -- title
-        local title = E.optionsPanel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge");
-        title:SetPoint("TOPLEFT", 16, -16);
-        title:SetText(E.AddOn .. ' Options');
+		-- Render options
+		local relative = title
+		for id=1, #options do
+			E.optionsPanel[options[id]["key"]]:SetPoint("TOPLEFT", relative, "LEFT", 0, -16)
+			E.optionsPanel[options[id]["key"]].text:SetText(options[id]["desc"])
+			E.optionsPanel[options[id]["key"]]:SetChecked(E:GetConfig(options[id]["key"]))
+			E.optionsPanel[options[id]["key"]]:SetScript("OnClick", function(event)
+				E:SaveConfig(options[id]["key"], event:GetChecked())
+			end)
+			relative = E.optionsPanel[options[id]["key"]]
+		end
 
-        -- render options
-        local relative = title
-        for id=1, #options do
-            E.optionsPanel[options[id]["key"]]:SetPoint("TOPLEFT", relative, "LEFT", 0, -16);
-            E.optionsPanel[options[id]["key"]].text:SetText(options[id]["desc"]);
-            E.optionsPanel[options[id]["key"]]:SetChecked(E:GetConfig(options[id]["key"]));
-            E.optionsPanel[options[id]["key"]]:SetScript("OnClick", function(event)
-                E:SaveConfig(options[id]["key"], event:GetChecked())
-            end)
-            relative = E.optionsPanel[options[id]["key"]]
-        end
+		-- Unregister to avoid repeated rendering of the addon options
+		self:SetScript("OnShow", nil)
+	end)
 
-        -- unregister so we dont repeatedly
-        -- render the addon options
-        self:SetScript("OnShow", nil)
-    end)
+	-- Register the options panel using the new Settings API
+	local function addOptionsCategory()
+		Settings.RegisterAddOnCategory(E.optionsPanel)
+	end
 
-    InterfaceOptions_AddCategory(E.optionsPanel)
+	if C_AddOns.IsAddOnLoaded("Blizzard_OptionsUI") then
+		addOptionsCategory()
+	else
+		local f = CreateFrame("Frame")
+		f:RegisterEvent("ADDON_LOADED")
+		f:SetScript("OnEvent", function(_, _, addon)
+			if addon == "Blizzard_OptionsUI" then
+				f:UnregisterEvent("ADDON_LOADED")
+				addOptionsCategory()
+			end
+		end)
+	end
 end
